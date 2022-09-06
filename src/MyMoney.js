bae862239d5c188bf7e6355224d2b8d32b32859f
Lootiano <lucianograbin12@gmail.com>
2022-09-06 05:22:51 -0300

    Alteração de cores e alterando os inputs por botões

--- src/MyMoney.js
+++ src/MyMoney.js
@@ -1,15 +1,35 @@
 var additionValueDisplay = document.getElementById("addition");
-var onclicked = additionValueDisplay.addEventListener(
+var onclickAddition = additionValueDisplay.addEventListener(
   "click",
   (btnAddition = () => {
     var inputValue = document.getElementById("value");
     if (inputValue.value != 0) {
-      document.getElementById("list").innerHTML += `
-    <div id="bar-green">
-      <h4>Valor adicionado</h4>
-      <p>${inputValue.value}</p>
-    </div>
-  `;
+      document.getElementById("input-list").innerHTML += `
+        <div id="partition">
+          <div id="partition-green">
+            <h4>Valor adicionado</h4>
+            <p>${inputValue.value}</p>
+          </div>
+        </div>
+      `;
+    }
+  })
+);
+
+var subtractionValueDisplay = document.getElementById("subtraction");
+var onclickSubtraction = subtractionValueDisplay.addEventListener(
+  "click",
+  (btnSubtraction = () => {
+    var inputValue = document.getElementById("value");
+    if (inputValue.value != 0) {
+      document.getElementById("input-list").innerHTML += `
+        <div id="partition">
+          <div id="partition-red">
+            <h4>Valor retirado</h4>
+            <p>${inputValue.value}</p>
+          </div>
+        </div>
+      `;
     }
   })
 );
