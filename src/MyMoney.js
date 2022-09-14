e64ff7ba0c6487e3d34714b279d0c210099e56f6
Lootiano <lucianograbin12@gmail.com>
2022-09-13 21:40:49 -0300

    Finalizando interações dos botões

--- src/MyMoney.js
+++ src/MyMoney.js
@@ -2,55 +2,55 @@ var inputValueTrash = document.getElementById("value");
 var btnAddition = document.getElementById("addition");
 var btnSubtraction = document.getElementById("subtraction");
 
-function inputVerification() {
-  var time = 0;
-  if (inputValueTrash.value < time) {
-    btnAddition.addEventListener("click", additionValue());
-  } else if (inputValueTrash.value > time) {
-    btnAddition.addEventListener("click", subtractionValue());
-  } else {
-    btnAddition.addEventListener("click", noValue());
-  }
-  console.log(inputValueTrash.value);
-}
+btnAddition.addEventListener(
+  "click",
+  (btnAddition = () => {
+    if (inputValueTrash.value == 0) {
+      valueIsNaN();
+    } else {
+      var partitionDiv = document.getElementById("input-list");
+      partitionDiv.innerHTML += `
+      <div id="partition">
+        <div id="partition-green">
+          <h3>Valor adicionado</h3>
+          <p>Valor: ${inputValueTrash.value}</p>
+        </div>
+      </div>
+    `;
+    }
+  })
+);
 
-function additionValue() {
-  var listValue = document.getElementById("input-list");
-  listValue.innerHTML += `
-    <div id="partition">
-      <div id="partition-green">
-        <h4>Valor adicionado</h4>
-        <p>${inputValueTrash.value}</p>
-      </div> 
-    </div>
-  `;
-}
+btnSubtraction.addEventListener(
+  "click",
+  (btnSubtraction = () => {
+    if (inputValueTrash.value == 0) {
+      valueIsNaN();
+    } else {
+      var partitionDiv = document.getElementById("input-list");
+      partitionDiv.innerHTML += `
+        <div id="partition">
+          <div id="partition-red">
+            <h4>Valor retirado</h4>
+            <p>Valor: ${inputValueTrash.value}</p>
+          </div>
+        </div>
+      `;
+    }
+  })
+);
 
-function subtractionValue() {
-  var listValue = document.getElementById("input-list");
-  listValue.innerHTML += `
-    <div id="partition">
-      <div id="partition-red">
-        <h4>Valor retirado</h4>
-        <p>${inputValueTrash.value}</p>
-      </div> 
-    </div>
-  `;
-}
-
-function noValue() {
-  var noValueDiv = document.getElementById("input-list");
-  noValueDiv.innerHTML += `
-    <div id="partition-clear">
+function valueIsNaN() {
+  var divNoValue = document.getElementById("input-list");
+  divNoValue.innerHTML = `
+    <div id="partition-clear" class="partition-clear">
       <div id="partition-value">
-        <h3>Nenhum valor especificado</h3>
-      </div> 
+        <h4>Valor não digitado</h4>
+      </div>
     </div>
   `;
-  for (noValue = 0; noValue < 0; ++i) {
-    setTimeout(() => {
-      var deleteDiv = document.getElementById("partition-clear");
-      deleteDiv.style.display = "none";
-    }, 1500);
-  }
+  setTimeout(() => {
+    const deletDiv = document.getElementById("partition-clear");
+    deletDiv.style.display = "none";
+  }, 1700);
 }
