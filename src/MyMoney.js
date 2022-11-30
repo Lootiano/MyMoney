ef387a589bf63b1cbe34368dcf599ea8edc5576a
Lootiano <lucianograbin12@gmail.com>
2022-11-30 00:06:56 -0300

    Adicionando a subtração de valores e melhorando o script

--- src/MyMoney.js
+++ src/MyMoney.js
@@ -1,22 +1,33 @@
-var moneyValue = JSON.parse(localStorage.getItem('value'));
-if (moneyValue == null) {
+getRegisterValue = () => {
+  return JSON.parse(localStorage.getItem('value'));
+};
+
+if (getRegisterValue() == null) {
   localStorage.setItem('value', JSON.stringify(0));
 }
+
+getValue = () => {
+  return document.getElementById('value').value;
+};
+
 getAddValue = () => {
-  let divGetValue = document.getElementById('value').value;
-  return divGetValue == ''
+  getValue() == ''
     ? alert('Valor não especificado')
-    : registerAddValue(divGetValue);
+    : registerAddValue(getValue());
+};
+
+getSubValue = () => {
+  getValue() == ''
+    ? alert('Valor não especificado')
+    : registerSubValue(getValue());
 };
 
 registerAddValue = (value) => {
-  let getValue = JSON.parse(localStorage.getItem('value'));
-  if (value == getValue) {
-    let con = confirm('Você tem certeza que quer registrar este número?');
-    con
-      ? localStorage.setItem('value', JSON.stringify(value))
-      : console.log(value);
-  }
-  localStorage.setItem('value', JSON.stringify(value));
-  moneyValue + value;
+  let addValue = JSON.parse(getRegisterValue()) + JSON.parse(value);
+  localStorage.setItem('value', JSON.stringify(addValue));
+};
+
+registerSubValue = (value) => {
+  let subValue = JSON.parse(getRegisterValue()) - JSON.parse(value);
+  localStorage.setItem('value', JSON.stringify(subValue));
 };
