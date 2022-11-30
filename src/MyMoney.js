cf8a2231f17cade2251cee8736eb7873ef4c167c
Lootiano <lucianograbin12@gmail.com>
2022-11-29 23:21:46 -0300

    Melhorando a funcionalidade

--- src/MyMoney.js
+++ src/MyMoney.js
@@ -1,56 +1,22 @@
-var inputValueTrash = document.getElementById("value");
-var btnAddition = document.getElementById("addition");
-var btnSubtraction = document.getElementById("subtraction");
-
-btnAddition.addEventListener(
-  "click",
-  (btnAddition = () => {
-    if (inputValueTrash.value == 0) {
-      valueIsNaN();
-    } else {
-      var partitionDiv = document.getElementById("input-list");
-      partitionDiv.innerHTML += `
-      <div id="partition">
-        <div id="partition-green">
-          <h3>Valor adicionado</h3>
-          <p>Valor: ${inputValueTrash.value}</p>
-        </div>
-      </div>
-    `;
-    }
-  })
-);
-
-btnSubtraction.addEventListener(
-  "click",
-  (btnSubtraction = () => {
-    if (inputValueTrash.value == 0) {
-      valueIsNaN();
-    } else {
-      var partitionDiv = document.getElementById("input-list");
-      partitionDiv.innerHTML += `
-        <div id="partition">
-          <div id="partition-red">
-            <h4>Valor retirado</h4>
-            <p>Valor: ${inputValueTrash.value}</p>
-          </div>
-        </div>
-      `;
-    }
-  })
-);
-
-function valueIsNaN() {
-  var divNoValue = document.getElementById("input-list");
-  divNoValue.innerHTML = `
-    <div id="partition-clear" class="partition-clear">
-      <div id="partition-value">
-        <h4>Valor não digitado</h4>
-      </div>
-    </div>
-  `;
-  setTimeout(() => {
-    const deletDiv = document.getElementById("partition-clear");
-    deletDiv.style.display = "none";
-  }, 1700);
+var moneyValue = JSON.parse(localStorage.getItem('value'));
+if (moneyValue == null) {
+  localStorage.setItem('value', JSON.stringify(0));
 }
+getAddValue = () => {
+  let divGetValue = document.getElementById('value').value;
+  return divGetValue == ''
+    ? alert('Valor não especificado')
+    : registerAddValue(divGetValue);
+};
+
+registerAddValue = (value) => {
+  let getValue = JSON.parse(localStorage.getItem('value'));
+  if (value == getValue) {
+    let con = confirm('Você tem certeza que quer registrar este número?');
+    con
+      ? localStorage.setItem('value', JSON.stringify(value))
+      : console.log(value);
+  }
+  localStorage.setItem('value', JSON.stringify(value));
+  moneyValue + value;
+};
